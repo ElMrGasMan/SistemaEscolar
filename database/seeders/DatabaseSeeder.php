636aa4c3e34e21d6bd5fb181ec5891fe1d44d509
Agustin Schipani <agus-calderon-schip03@outlook.com>
2024-11-25 18:40:08 -0300

    Añado lo de placido
    Ojala no se rompa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,19 @@
 <?php
-
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+public function run()
+{
+$this->call([
+SubjectsTableSeeder::class,
+CoursesTableSeeder::class,
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+StudentsTableSeeder::class,
+Course_studentSeeder::class,
+ProfessorsTableSeeder::class,
+
+CommissionsTableSeeder::class,
+]);
+}
 }
